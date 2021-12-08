9c8bcfe88456859f01eda554e76cbb1b58db7f4e
Vitor Hugo de Souza <39071124+VitorHugoSouza@users.noreply.github.com>
2021-12-07 23:35:14 -0300

    Update Monitoramento.js

--- src/monitoramento/Monitoramento.js
+++ src/monitoramento/Monitoramento.js
@@ -9,23 +9,8 @@ import './Monitoramento.css';
 
 export default function Monitoramento() {
 
-	const [controle, setControle] = useState(5.2);
-	const [estado, setEstado] = useState(true);
-	const [volume, setVolume] = useState(5);
-	const [placa, setPlaca] = useState([]);
 	const [dadosPlaca1, setDadosPlaca1] = useState([]);
-	const [distancia, setDistancia] = useState(0);
-
-	function getDistancia() {
-		const distanciaRef = ref(db, 'distancia');
-		
-		return onValue(distanciaRef, (distanciaCol) => {
-			if(distanciaCol.exists()) {
-				console.log('distancia atualizada ==>', distanciaCol.val());
-				setDistancia(distanciaCol.val());
-			}
-		});
-	}	
+	const [dadosPlaca2, setDadosPlaca2] = useState([]);
 
 	function getPlaca1() {
 		const distanciaRef = ref(db, 'placa-1');
@@ -37,23 +22,33 @@ export default function Monitoramento() {
 			}
 		});
 	}	
+
+	function getPlaca2() {
+		const distanciaRef = ref(db, 'placa-2');
+		
+		return onValue(distanciaRef, (placa) => {
+			if(placa.exists()) {
+				console.log('placa2 atualizada ==>', placa.val());
+				setDadosPlaca2(placa.val());
+			}
+		});
+	}	
 	
 	useEffect(() => {
-		getDistancia();	
-		getPlaca1();	
+
+		getPlaca1();
+		getPlaca2();
+
   }, []);
 
 	return (
 		<div>
 
-			<h1>Distância atualizada: {distancia}</h1>
-
-			<h1>Placa 1: {dadosPlaca1.distancia}</h1>
-			<h1>Status: {dadosPlaca1.status}</h1>
-
 			<h3>Bem vindo ao sistema de monitoramento online!</h3>
 
-			<div style={{ marginTop: '30px'}}>			
+			<div style={{ marginTop: '30px'}}>	
+
+				<h3>Placa primária </h3>	
 
 				<Row gutter={16}>
 
@@ -66,7 +61,7 @@ export default function Monitoramento() {
 								style={{ marginBottom: '10px' }}
 							/>
 
-							{controle <= 5 ? <p style={{ color: 'red' }}><b>Volume inferior a 5 m³. Nivel crítico!</b></p> : <p style={{ color: 'green' }}>Volume em bom nível!</p>}
+							{dadosPlaca1.distancia <= 5 ? <p style={{ color: 'red' }}><b>Volume inferior a 5 m³. Nivel crítico!</b></p> : <p style={{ color: 'green' }}>Volume em bom nível!</p>}
 
 						</Card>
 					</Col>
@@ -79,10 +74,10 @@ export default function Monitoramento() {
 								style={{ marginBottom: '10px' }}
 							/>
 
-							{estado === true ? 
-								<p style={{ color: 'green' }}>Lampada acesa. Temperatura controlada!</p>							
+							{dadosPlaca1.luminosidade >= 750 ? 
+								<p style={{ color: 'green' }}>Lâmpada acesa. Temperatura controlada!</p>							
 								:
-								<p style={{ color: 'red' }}>Lampada apagada. Verifique a temperatura!</p> 
+								<p style={{ color: 'red' }}>Lâmpada apagada. Verifique a placa!</p> 
 							}
 							
 
@@ -93,11 +88,11 @@ export default function Monitoramento() {
 						<Card style={{ width: 300 }} bordered={true}>
 							<Meta
 								avatar={<RedoOutlined />}
-								title="Volume Atual do Silo"
+								title="Volume Atual"
 								style={{ marginBottom: '10px' }}
 							/>
 
-							<p>Volume em {volume}m³.</p>
+							<p>Volume em {dadosPlaca1.distancia}m³.</p>
 
 						</Card>
 					</Col>
@@ -106,18 +101,60 @@ export default function Monitoramento() {
 			</div>
 
 			<Divider />
-			
-			<div>
-				<p><b>Informações das placas conectadas</b></p>
-			</div>
 
-			<div>
-				<label>PLACA 1 - ON</label>
-			</div>
+			<div style={{ marginTop: '30px'}}>	
 
-			<div>
-				<label>PLACA 2 - ON</label>
-			</div>
+				<h3>Placa secundária</h3>		
+
+				<Row gutter={16}>
+
+
+					<Col span={8}>
+						<Card style={{ width: 300 }} bordered={true}>
+							<Meta
+								avatar={<FormOutlined />}
+								title="Controle de volume"
+								style={{ marginBottom: '10px' }}
+							/>
+
+							{dadosPlaca2.distancia <= 5 ? <p style={{ color: 'red' }}><b>Volume inferior a 5 m³. Nivel crítico!</b></p> : <p style={{ color: 'green' }}>Volume em bom nível!</p>}
+
+						</Card>
+					</Col>
+
+					<Col span={8}>
+						<Card style={{ width: 300 }} bordered={true}>
+							<Meta
+								avatar={<BulbOutlined />}
+								title="Estado lâmpada"
+								style={{ marginBottom: '10px' }}
+							/>
+
+							{dadosPlaca2.luminosidade <= 100 ? 
+								<p style={{ color: 'green' }}>Lâmpada acesa. Temperatura controlada!</p>							
+								:
+								<p style={{ color: 'red' }}>Lâmpada apagada. Verifique a placa!</p> 
+							}
+							
+
+						</Card>
+					</Col>
+
+					<Col span={8}>
+						<Card style={{ width: 300 }} bordered={true}>
+							<Meta
+								avatar={<RedoOutlined />}
+								title="Volume Atual"
+								style={{ marginBottom: '10px' }}
+							/>
+
+							<p>Volume em {dadosPlaca2.distancia}m³.</p>
+
+						</Card>
+					</Col>
+
+				</Row>
+			</div>		
 
 		</div>
 	);
