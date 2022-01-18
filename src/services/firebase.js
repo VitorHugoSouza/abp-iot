773412678227d05adaa06bae8c0c9e32bc944c2a
Vitor Hugo de Souza <39071124+VitorHugoSouza@users.noreply.github.com>
2022-01-18 14:23:53 -0300

    update service firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase } from "firebase/database";
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDqoJQHMFDfhTV2FnIVTNSKnImpHLfZcp8",
@@ -15,39 +15,3 @@ const fire = initializeApp(firebaseConfig);
 const db = getDatabase(fire);
 
 export { fire, db };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*/ Import the firebase package downloaded to this project folder through npm
-import firebase from "firebase";
-
-// Define a variable of the project name, which is used in the config parameters for firebase
-const firebaseProjectName = "sys-monitoramento"
-
-// Parameters required by the initializeApp used below
-const config = {
-  apiKey: "<YOUR_WEB_API_KEY>",
-  authDomain: `${firebaseProjectName}.firebaseapp.com`,
-  databaseURL: `https://${firebaseProjectName}.firebaseio.com`,
-  projectId: `${firebaseProjectName}`
-};
-
-firebase.initializeApp(config);
-
-export default firebase;*/
